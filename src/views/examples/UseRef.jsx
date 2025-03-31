91dd07349fa895cbe6294cea33b70886b1434045
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-31 12:11:30 -0300

    feat: complete useRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseRef = (props) => {
+const UseRef = () => {
+    const [val, setVal] = useState("")
+    const changeCount = useRef(0) 
+    const inputRef = useRef(null)
+
+    const focusInput = () => {
+        inputRef.current.focus()
+    }
+
+    const handleChange = (e) => {
+        setVal(e.target.value)
+        changeCount.current += 1 
+    }
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+
+            <div>
+                <input 
+                    ref={inputRef} 
+                    className='input' 
+                    value={val} 
+                    onChange={handleChange}
+                />
+                <button className="btn" onClick={focusInput}>Focar</button>
+                <p>Alterações: {changeCount.current}</p> {}
+            </div>
         </div>
     )
 }
