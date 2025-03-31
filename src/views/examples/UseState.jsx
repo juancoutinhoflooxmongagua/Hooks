8f978ac4ee413cad645956ab105403654738b5b9
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-31 11:29:50 -0300

    feat: create sctruture for UseState

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
+import SectionTitle from '../../components/layout/SectionTitle'
 const UseState = (props) => {
+    const [count, setCound] = useState(0)
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+    
+            <SectionTitle></SectionTitle>
         </div>
     )
 }
