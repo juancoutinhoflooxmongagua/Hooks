8ced889aebd830e1f7f3d252c37a6c7747d91b40
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 08:19:15 -0300

    feat: create sctruture for use memo

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseMemo = (props) => {
+    const [n1, setN1] = useState(0)
+    const [n2, setN2] = useState(0)
+    const [n3, setN3] = useState(0)
+
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
                 subtitle="Retorna um valor memoizado!"
             />
+
+            <div className='center'>
+                <input type="number" className="input" value={n1} onChange={e => setN1(parseInt(e.target.value))} />
+            </div>
         </div>
     )
 }
