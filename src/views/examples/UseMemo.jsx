6acef8dd0a6ca64dff9e710c72597e08c4587bf7
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 08:28:32 -0300

    feat: add useMemo()

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 function sum(a, b) {
     const future = Date.now() + 2000
-    while(Date.now() < future) {}
+    while (Date.now() < future) {}
     return a + b
 }
+
 const UseMemo = (props) => {
     const [n1, setN1] = useState(1)
     const [n2, setN2] = useState(2)
     const [n3, setN3] = useState(0)
 
-    const [result, setResult] = useState(0)
-
-    useEffect(function() {
-        setResult(sum(n1, n2))
-    }, [n1, n2])
-
-
+    const result = useMemo(() => sum(n1, n2), [n1, n2])
 
     return (
         <div className="UseMemo">
@@ -26,11 +21,11 @@ const UseMemo = (props) => {
                 subtitle="Retorna um valor memoizado!"
             />
 
-            <span>Resultado: {resultado}</span>
+            <span>Resultado: {result}</span>
             <div className='center'>
-                <input type="number" className="input" value={n1} onChange={e => setN1(parseInt(e.target.value))} />
-                <input type="number" className="input" value={n2} onChange={e => setN2(parseInt(e.target.value))} />
-                <input type="number" className="input" value={n3} onChange={e => setN3(parseInt(e.target.value))} />
+                <input type="number" className="input" value={n1} onChange={e => setN1(parseInt(e.target.value) || 0)} />
+                <input type="number" className="input" value={n2} onChange={e => setN2(parseInt(e.target.value) || 0)} />
+                <input type="number" className="input" value={n3} onChange={e => setN3(parseInt(e.target.value) || 0)} />
             </div>
         </div>
     )
