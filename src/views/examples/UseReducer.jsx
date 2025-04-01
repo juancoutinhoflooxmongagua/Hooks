e70b377502482ed49953fa3a2088685f86a24994
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 09:36:35 -0300

    feat: finish useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseReducer = (props) => {
+const estadoInicial = { contador: 0 }
+
+function redutor(estado, acao) {
+    switch (acao.tipo) {
+        case 'incrementar':
+            return { contador: estado.contador + 2 } 
+        case 'multiplicar':
+            return { contador: estado.contador * 7 } 
+        case 'dividir':
+            return { contador: estado.contador / 25 } 
+        case 'decrementar':
+            return { contador: estado.contador - 1 } 
+        default:
+            throw new Error('Ação desconhecida')
+    }
+}
+
+const UseReducer = () => {
+    const [estado, despachar] = useReducer(redutor, estadoInicial)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+
+            <div>
+                <span>
+                    Contador: {estado.contador}
+                </span>
+
+                <div>
+                    <button onClick={() => despachar({ tipo: 'incrementar' })} className="btn">+2</button>
+                    <button onClick={() => despachar({ tipo: 'multiplicar' })} className="btn">*7</button>
+                    <button onClick={() => despachar({ tipo: 'dividir' })} className="btn">/25</button>
+                    <button onClick={() => despachar({ tipo: 'decrementar' })} className="btn">-1</button>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default UseReducer
+export default React.memo(UseReducer)
