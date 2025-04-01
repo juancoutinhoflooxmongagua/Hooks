bf0027671e8417a7feb1874f1a172da69c08ccd2
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 08:34:11 -0300

    feat: create functions for UseCallback

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseCallback = (props) => {
+const UseCallback = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = useCallback(() => {
+        setCount(prevCount => prevCount + 1)
+    }, [])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <div className="center">
+                <span className="text">Contador: {count}</span>
+                <button className="btn" onClick={increment}>Incrementar</button>
+            </div>
         </div>
     )
 }
